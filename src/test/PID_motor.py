3763ec9138b30d98ab6324afc3afef578fe761a8
AshtonRopp <ashton.ropp@gmail.com>
2025-12-06 13:34:41 -0500

    First draft of PID_motor.py

---
+++ src/test/PID_motor.py
@@ -0,0 +1,226 @@
+import time
+
+import smbus					#import SMBus module of I2C
+import math
+import pigpio
+
+# from gpiozero import Servo
+import gpiozero
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+gpiozero.Device.pin_factory = PiGPIOFactory('127.0.0.1')
+from gpiozero import AngularServo
+
+#some MPU6050 Registers and their Address
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
+
+
+def MPU_Init():
+	#write to sample rate register
+	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+	
+	#Write to power management register
+	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+	
+	#Write to Configuration register
+	bus.write_byte_data(Device_Address, CONFIG, 0)
+	
+	#Write to Gyro configuration register
+	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+	
+	#Write to interrupt enable register
+	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+
+def read_raw_data(addr):
+	#Accelero and Gyro value are 16-bit
+	high = bus.read_byte_data(Device_Address, addr)
+	low = bus.read_byte_data(Device_Address, addr+1)
+
+	#concatenate higher and lower value
+	value = ((high << 8) | low)
+	
+	#to get signed value from mpu6050
+	if(value > 32768):
+		value = value - 65536
+	return value
+
+
+
+bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+Device_Address = 0x68   # MPU6050 device address
+
+MPU_Init()
+
+# servoPIN_n = 17
+# servoPIN_e = 23
+# servoPIN_s = 22
+# servoPIN_w = 27
+# prevN = 0.0 #+y
+# prevS = 0.0 #-y
+# prevE = 0.0	#+x
+# prevW = 0.0	#-x
+
+# currN = 0.0
+# currS = 0.0
+# currE = 0.0
+# currW = 0.0
+
+# pwm = pigpio.pi()
+# pwm.set_mode(servoPIN_w, pigpio.OUTPUT)
+# pwm.set_PWM_frequency( servoPIN_w, 50 )
+# pwm.set_mode(servoPIN_e, pigpio.OUTPUT)
+# pwm.set_PWM_frequency( servoPIN_e, 50 )
+
+
+
+# # angles = [[0, 30], [0, 30], [?]]
+# servoN = AngularServo(servoPIN_n, initial_angle=0, min_angle=0, max_angle=180, min_pulse_width=0.0004, max_pulse_width=0.0026)
+# servoE = AngularServo(servoPIN_e, initial_angle=0, min_angle=0, max_angle=180, min_pulse_width=0.0004, max_pulse_width=0.0026)
+# servoS = AngularServo(servoPIN_s, initial_angle=0, min_angle=0, max_angle=180, min_pulse_width=0.0004, max_pulse_width=0.0026)
+# # servoW = AngularServo(servoPIN_w, initial_angle=0, min_angle=0, max_angle=180, min_pulse_width=0.0004, max_pulse_width=0.0026)
+
+Gx_offset = -0.28100763358778624
+Gy_offset = 0.0648854961832061
+Gz_offset = 0.09798473282442748
+Ax_moffset = -0.99430131
+Ax_boffset = 0.02041231
+Ay_moffset = -0.99869671
+Ay_boffset = 0.02664353
+Az_moffset = -0.99757678
+Az_boffset = 0.0469103
+
+# Comparison coeffs
+# Gx_offset = 0
+# Gy_offset = 0
+# Gz_offset = 0
+# Ax_moffset = 1
+# Ax_boffset = 0
+# Ay_moffset = 1
+# Ay_boffset = 0
+# Az_moffset = 1
+# Az_boffset = 0
+
+
+print (" Reading Data of Gyroscope and Accelerometer")
+
+try:
+
+	
+	kp = 10
+	ki = 0.3
+	kd = 0.01
+	iS = 0.0
+	iN = 0.0
+	iW = 0.0
+	iE = 0.0
+	intgralS = 0.0
+	intgralN = 0.0
+	intgralW = 0.0
+	intgralE = 0.0
+
+	iteration_time = 0.1
+	YZ_integral = 0
+	YZ_error_prior = 0
+
+	while True:
+		
+		#Read Accelerometer raw value
+		acc_x = read_raw_data(ACCEL_XOUT_H)
+		acc_y = read_raw_data(ACCEL_YOUT_H)
+		acc_z = read_raw_data(ACCEL_ZOUT_H)
+
+		#Read Gyroscope raw value
+		gyro_x = read_raw_data(GYRO_XOUT_H)
+		gyro_y = read_raw_data(GYRO_YOUT_H)
+		gyro_z = read_raw_data(GYRO_ZOUT_H)
+
+		#Full scale range +/- 250 degree/C as per sensitivity scale factor
+		Ax = acc_x/16384.0
+		Ay = acc_y/16384.0
+		Az = acc_z/16384.0
+
+
+		Gx = gyro_x/131.0
+		Gy = gyro_y/131.0
+		Gz = gyro_z/131.0
+
+		Ax = (Ax * Ax_moffset) + Ax_boffset
+		Ay = (Ay * Ay_moffset) + Ay_boffset
+		Az = (Az * Az_moffset) + Az_boffset
+
+		Gx -= Gx_offset
+		Gy -= Gy_offset
+		Gz -= Gz_offset
+
+		XZ_angle = math.atan(Ax/Az)
+		YZ_angle = math.atan(Ay/Az)
+		
+
+		# print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.8f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
+		print ("Ax=%.8f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
+		
+		time.sleep(.1)
+
+
+
+		YZ_error = -0.067 - (-YZ_angle)
+		YZ_integral += YZ_error * iteration_time
+		YZ_derivative = (YZ_error - YZ_error_prior) / iteration_time
+		YZ_output = kp*YZ_error + ki*YZ_integral + kd*YZ_derivative + 0
+		YZ_error_prior = YZ_error
+		# print(kp*YZ_error, ki*YZ_integral, kd*YZ_derivative, YZ_output)
+		# print((math.degrees(-YZ_angle)+360), YZ_output, 0.5*(math.degrees(YZ_output)+360), 1500+YZ_output*2000/8, 1500-YZ_output*2000/8)
+		# pwm.set_servo_pulsewidth( servoPIN_w, min(max(1500+YZ_output*2000/8, 1000), 2000))
+		# pwm.set_servo_pulsewidth( servoPIN_e, min(max(1500-YZ_output*2000/8, 1000), 2000))
+		
+		# time.sleep( 3 )
+
+		#TODO:
+		# fix servos to test if we want kd or not
+		# Update pid code for other servos and clean up formatting
+		# Fix git repo
+
+		# PID no longer works?
+		# west servo PID works but offset breaks east servo
+		# south servo is broke
+		# now, none are working???
+
+		# print(-YZ_angle)
+		# servoS.angle = math.degrees(output)
+
+
+		# propN = Ax*kp
+		# derN = kd*(currN - prevN)/0.1
+		# iN -= Ax
+		# intgralN -= iN*0.1
+		# servoN.angle = propN + derN + (ki * intgralN)
+
+		# propE = Ay * kp
+		# derE = kd*(currE - prevE) / 0.1
+		# iE -= Ay
+		# intgralE -= iE * 0.1
+		# servoE.angle = propE + derE + (ki * intgralE)
+
+		# propW = -Ay * kp
+		# derW = kd*(currW - prevW) / 0.1
+		# iW += Ay
+		# intgralW += iW * 0.1
+		# servoW.angle = propW + derW + (ki * intgralW)
+
+except KeyboardInterrupt:
+	# pwm.set_PWM_dutycycle( servoPIN_w, 0 )
+	# pwm.set_PWM_frequency( servoPIN_w, 0 )
+
+	# pwm.set_PWM_dutycycle( servoPIN_e, 0 )
+	# pwm.set_PWM_frequency( servoPIN_e, 0 )
+	print("stop")
